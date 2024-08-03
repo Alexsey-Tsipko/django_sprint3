b98dabdc1b72bb8a284ed2be3270d2e4fded08a7
Alexsey-Tsipko <popolo090909@yandex.ru>
2024-08-03 12:54:54 +0300

    03082024

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -20,7 +20,7 @@ def post_detail(request: HttpRequest, post_id: int) -> HttpResponse:
 
 def category_posts(request, category_slug):
     posts = Post.objects.filter(category__slug=category_slug)
-    category = get_object_or_404(Category, slug=category_slug)
+    category = get_object_or_404(Category, slug=category_slug, is_published=True)
     location_name = posts.first().location.name if posts.exists() and posts.first().location else "Планета Земля"
     sorted_posts = [post for post in posts if post.category.slug
                     == category_slug]
