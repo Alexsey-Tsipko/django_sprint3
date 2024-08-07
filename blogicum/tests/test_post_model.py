c8bfca66d69201bd4477f3c618f89e7370d41a5c
Alexsey-Tsipko <popolo090909@yandex.ru>
2024-08-07 21:11:16 +0300

    03082024-12

--- blogicum/tests/test_post_model.py
+++ blogicum/tests/test_post_model.py
@@ -1,10 +1,11 @@
 import pytest
 from django.db.models import (
-    BooleanField, CharField, DateTimeField, ForeignKey, TextField)
+    BooleanField, CharField, DateTimeField,
+    ForeignKey, TextField)
 from django.db.utils import IntegrityError
 
 from blog.models import Post
-from blogicum.tests.conftest import _TestModelAttrs
+from tests.conftest import _TestModelAttrs
 
 pytestmark = [
     pytest.mark.django_db,
@@ -13,14 +14,22 @@ pytestmark = [
 
 @pytest.mark.parametrize(
     ('field', 'type', 'params'), [
-        ('title', CharField, {'max_length': 256}),
+        ('title', CharField, {
+            'max_length': 256}),
         ('text', TextField, {}),
-        ('pub_date', DateTimeField, {'auto_now': False, 'auto_now_add': False}),
-        ('author', ForeignKey, {'null': False}),
-        ('location', ForeignKey, {'null': True}),
-        ('category', ForeignKey, {'null': True, 'blank': False}),
-        ('is_published', BooleanField, {'default': True}),
-        ('created_at', DateTimeField, {'auto_now_add': True}),
+        ('pub_date', DateTimeField, {
+            'auto_now': False,
+            'auto_now_add': False}),
+        ('author', ForeignKey, {
+            'null': False}),
+        ('location', ForeignKey, {
+            'null': True}),
+        ('category', ForeignKey, {
+            'null': True, 'blank': False}),
+        ('is_published', BooleanField, {
+            'default': True}),
+        ('created_at', DateTimeField, {
+            'auto_now_add': True}),
     ])
 class TestPostModelAttrs(_TestModelAttrs):
 
@@ -35,25 +44,33 @@ def test_author_on_delete(posts_with_author):
         author.delete()
     except IntegrityError:
         raise AssertionError(
-            'Проверьте, что значение атрибута `on_delete` '
-            'поля `author` в модели `Post` соответствует заданию.'
+            'Проверьте, что значение атрибута '
+            'on_delete поля author в модели Post '
+            'соответствует заданию.'
         )
-    assert not Post.objects.filter(author=author).exists(), (
-        'Проверьте, что значение атрибута `on_delete` '
-        'поля `author` в модели `Post` соответствует заданию.'
+    assert not Post.objects.filter(
+        author=author).exists(), (
+        'Проверьте, что значение атрибута '
+        'on_delete поля author в модели Post '
+        'соответствует заданию.'
     )
 
 
-def test_location_on_delete(posts_with_published_locations):
-    location = posts_with_published_locations[0].location
+def test_location_on_delete(
+        posts_with_published_locations):
+    location = (
+        posts_with_published_locations[0].location)
     try:
         location.delete()
     except IntegrityError:
         raise AssertionError(
-            'Проверьте, что значение атрибута `on_delete` '
-            'поля `location` в модели `Post` соответствует заданию.'
+            'Проверьте, что значение атрибута '
+            'on_delete поля location в модели Post '
+            'соответствует заданию.'
         )
-    assert Post.objects.filter(location=location).exists(), (
-        'Проверьте, что значение атрибута `on_delete` '
-        'поля `location` в модели `Post` соответствует заданию.'
+    assert Post.objects.filter(
+        location=location).exists(), (
+        'Проверьте, что значение атрибута '
+        'on_delete поля location в модели Post '
+        'соответствует заданию.'
     )
